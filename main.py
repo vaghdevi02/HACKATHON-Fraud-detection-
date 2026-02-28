f68702c05e1d48b7e4592da7945559242f34613f
vaghdevi02 <vaghdevir@gmail.com>
2026-02-28 12:05:01 +0530

    main

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/predict")
+def predict(data: dict):
+    return {"result": "Fraud"}
